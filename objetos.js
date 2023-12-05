c2c4406ec9bf2219ed18ffab0d978b36f01ccb2f
elihotchillipeper <150726982+elihotchillipeper@users.noreply.github.com>
2023-12-05 16:55:00 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,68 @@
+var objCaes = [
+    {
+        imagem: "pet-img.png",
+        titulo: "Nome 1",
+        desc: "cachorro1 cachorro1 cachorro1 cachorro1 cachorro1",
+        genero: "fêmea",
+        categoria: "cachorro",
+    },
+    {
+        imagem: "pet-img.png",
+        titulo: "Nome 2",
+        desc: "cachorro2 cachorro2 cachorro2 cachorro2 cachorro2",
+        genero: "macho",
+        categoria: "cachorro",
+    },
+    {
+        imagem: "pet-img.png",
+        titulo: "Nome 3",
+        desc: "cachorro3 cachorro3 cachorro3 cachorro3 cachorro3",
+        genero: "macho",
+        categoria: "gato",
+    },
+    {
+        imagem: "pet-img.png",
+        titulo: "Nome 4",
+        desc: "cachorro4 cachorro4 cachorro4 cachorro4 cachorro4",
+        genero: "fêmea",
+        categoria: "gato",
+    },
+    {
+        imagem: "pet-img.png",
+        titulo: "Nome 5",
+        desc: "cachorro5 cachorro5 cachorro5 cachorro5 cachorro5",
+        genero: "fêmea",
+        categoria: "gato",
+    },
+    {
+        imagem: "pet-img.png",
+        titulo: "Nome 6",
+        desc: "cachorro6 cachorro6 cachorro6 cachorro6 cachorro6",
+        genero: "fêmea",
+        categoria: "gato",
+    },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
